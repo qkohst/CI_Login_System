27aa186d5a09874d6b2b012be8a93fa3a4c7ab4a
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2021-08-01 00:01:30 +0700

    Preparation & add Assets + Auth Page

---
+++ application/views/auth/registration.php
@@ -0,0 +1,41 @@
+<div class="authincation h-100">
+  <div class="container h-100">
+    <div class="row justify-content-center h-100 align-items-center">
+      <div class="col-md-6">
+        <div class="authincation-content">
+          <div class="row no-gutters">
+            <div class="col-xl-12">
+              <div class="auth-form">
+                <h4 class="text-center mb-4">Register your account</h4>
+                <form action="index.html">
+                  <div class="form-group">
+                    <label class="mb-1"><strong>Fullname</strong></label>
+                    <input type="text" class="form-control" placeholder="fullname" id="name" name="name">
+                  </div>
+                  <div class="form-group">
+                    <label class="mb-1"><strong>Email</strong></label>
+                    <input type="text" class="form-control" placeholder="hello@example.com" id="email" name="email">
+                  </div>
+                  <div class="form-group">
+                    <label class="mb-1"><strong>Password</strong></label>
+                    <input type="password" class="form-control" placeholder="password" id="password1" name="password1">
+                  </div>
+                  <div class="form-group">
+                    <label class="mb-1"><strong>Confirm Password</strong></label>
+                    <input type="password" class="form-control" placeholder="confirm password" id="password2" name="password2">
+                  </div>
+                  <div class="text-center mt-4">
+                    <button type="submit" class="btn btn-primary btn-block">Register</button>
+                  </div>
+                </form>
+                <div class="new-account mt-3">
+                  <p>Already have an account? <a class="text-primary" href="<?= base_url('auth'); ?>">Login</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ application/views/layouts/auth_header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>CodeIgniter Login System</title>
+  <!-- Favicon icon -->
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets'); ?>./images/favicon.png">
+  <link href="<?= base_url('assets'); ?>./css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="h-100">
