29c6eaf624c044492ed2f661d6f615eaaae5f01f
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2021-08-01 15:55:17 +0700

    Add Menu Management Part-1

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -4,18 +4,51 @@
     <div class="deznav">
       <div class="deznav-scroll">
         <ul class="metismenu" id="menu">
-          <li>
-            <a href="widget-basic.html" class="ai-icon" aria-expanded="false">
-              <i class="flaticon-381-networking"></i>
-              <span class="nav-text">Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a href="widget-basic.html" class="ai-icon" aria-expanded="false">
-              <i class="flaticon-381-user-1"></i>
-              <span class="nav-text">My Profile</span>
-            </a>
-          </li>
+
+          <!-- Query Menu  -->
+          <?php
+          $role_id = $this->session->userdata('role_id');
+          $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                          FROM `user_menu` 
+                          JOIN `user_access_menu` ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                        WHERE `user_access_menu`.`role_id` = $role_id
+                        ORDER BY `user_access_menu`.`menu_id` ASC
+                        ";
+          $menu = $this->db->query($queryMenu)->result_array();
+          ?>
+
+          <!-- Looping Menu  -->
+          <?php foreach ($menu as $m) : ?>
+            <span class="nav-text text-seconday ml-4">
+              <strong>
+                <?= $m['menu']; ?>
+              </strong>
+            </span>
+
+            <!-- Siapkan Sub Menu Sesuai Menu  -->
+            <?php
+            $menuId = $m['id'];
+            $querySubMenu = "SELECT *
+                              FROM `user_sub_menu` 
+                              JOIN `user_menu` ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                            WHERE `user_sub_menu`.`menu_id` = $menuId
+                            AND `user_sub_menu`.`is_active` = 1
+                            ";
+            $subMenu = $this->db->query($querySubMenu)->result_array();
+            ?>
+
+            <!-- Looping Sub Menu  -->
+            <?php foreach ($subMenu as $sm) : ?>
+              <li>
+                <a href="<?= base_url($sm['url']); ?>" class="ai-icon" aria-expanded="false">
+                  <i class="<?= $sm['icon']; ?>"></i>
+                  <span class="nav-text"><?= $sm['title']; ?></span>
+                </a>
+              </li>
+            <?php endforeach; ?>
+
+          <?php endforeach; ?>
+         
         </ul>
 
         <div class="copyright fixed-bottom">
